fee0399886dd01517e9b94e3ce99c09116f7c64e
Mimcha <40563006+Mimcha@users.noreply.github.com>
2024-10-11 09:05:31 +0300

    Add file server - client

---
+++ JCore_8_OSI/src/main/java/org/example/Server.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static final Integer PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Сервер запущен");
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ) {
+                    final String name = in.readLine();
+                    System.out.printf("New connection accepted. Name client: %s, port: %d %n", name, clientSocket.getPort());
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
